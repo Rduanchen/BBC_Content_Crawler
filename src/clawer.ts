8415922d9094fc583a782e8c3102dec1e5a2331f
Justin <chenyouduan@gmail.com>
2025-08-19 11:09:22 +0800

    refactor: enhance BBC clawer functionality with environment settings and image resolution

--- src/clawer.ts
+++ src/clawer.ts
@@ -1,9 +1,26 @@
 import axios from "axios";
+import { BBCEnviroment } from "./model.js";
 
-const BBC_TITLE_PAGE_URL = "https://www.bbc.com/news"; // Default BBC News title page URL
+let BBC_TITLE_PAGE_URL = "https://feeds.bbci.co.uk/news/rss.xml"; // Default BBC News title page URL
 // You can override this by setting the BBC_TITLE_PAGE_URL environment variable
-const BBC_ROOT_URL = "https://www.bbc.com"; // Default BBC root URL
+let BBC_ROOT_URL = "https://www.bbc.com"; // Default BBC root URL
 // You can override this by setting the BBC_ROOT_URL environment variable
+let BBC_IMAGE_RESOLUTION = 480; // Default BBC image resolution
+// You can override this by setting the BBC_IMAGE_RESOLUTION environment variable
+
+function imageResolutionSetting(resolution: "low" | "medium" | "high"): void {
+  switch (resolution) {
+    case "low":
+      BBC_IMAGE_RESOLUTION = 240;
+      break;
+    case "medium":
+      BBC_IMAGE_RESOLUTION = 480;
+      break;
+    case "high":
+      BBC_IMAGE_RESOLUTION = 1024;
+      break;
+  }
+}
 
 async function crawler(url: string): Promise<string> {
   try {
@@ -22,4 +39,20 @@ async function crawler(url: string): Promise<string> {
   }
 }
 
-export { crawler, BBC_TITLE_PAGE_URL, BBC_ROOT_URL };
+function setBBCEnvironment(BBCEnvironment: BBCEnviroment): void {
+  BBC_TITLE_PAGE_URL = BBCEnvironment.titlePageUrl
+    ? BBCEnvironment.titlePageUrl
+    : BBC_TITLE_PAGE_URL;
+  BBC_ROOT_URL = BBCEnvironment.rootUrl ? BBCEnvironment.rootUrl : BBC_ROOT_URL;
+  if (BBCEnvironment.imageResolution) {
+    imageResolutionSetting(BBCEnvironment.imageResolution);
+  }
+}
+
+export {
+  crawler,
+  BBC_TITLE_PAGE_URL,
+  BBC_ROOT_URL,
+  setBBCEnvironment,
+  BBC_IMAGE_RESOLUTION,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { getNewsTitle, getNewsContent } from "./function.js";
-import { crawler, BBC_TITLE_PAGE_URL } from "./clawer.js";
-import { NewsTitles, NewsContent } from "./model.js";
+import { crawler, BBC_TITLE_PAGE_URL, setBBCEnvironment } from "./clawer.js";
+import { NewsTitles, NewsContent, BBCEnviroment, BBCConfig } from "./model.js";
 
 async function getBBCNewsTitles(): Promise<NewsTitles[]> {
   const body = await crawler(BBC_TITLE_PAGE_URL);
@@ -12,7 +12,16 @@ async function getBBCNewsContent(url: string): Promise<NewsContent> {
   return getNewsContent(body);
 }
 
+function configBBC(config: BBCConfig = {}) {
+  setBBCEnvironment({
+    titlePageUrl: config.titlePageUrl || null,
+    rootUrl: config.rootUrl || null,
+    imageResolution: config.imageResolution || null,
+  });
+}
+
 export {
+  configBBC,
   getBBCNewsTitles,
   getBBCNewsContent,
   crawler,
